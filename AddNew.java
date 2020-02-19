f1c6a2b9d0f6f98de525847ad2b6fffd1b591ad0
melwinkle <50456098+melwinkle@users.noreply.github.com>
2020-02-19 15:34:42 +0000

    Update AddNew.java
    updated a method

--- AddNew.java
+++ AddNew.java
@@ -58,19 +58,39 @@ public class AddNew implements Initializable {
 
 
     public void buttonsave(ActionEvent actionEvent) throws IOException {
-        Parent screen_two= FXMLLoader.load(getClass().getResource("Gal_Ob.fxml"));
+        Parent screen_two= FXMLLoader.load(getClass().getResource("Add_New.fxml"));
         Scene scene = new Scene(screen_two);
         Stage s_two=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         s_two.setScene(scene);
         s_two.show();
         galam.setVegetation_color(veg_color.getText());
-        galam.setColor_value(Integer.parseInt(colour_value.getText()));
+        galam.setColor_value(Integer.parseInt(color_value.getText()));
         galam.setLatitude(Double.parseDouble(pos_lat.getText()));
         galam.setLongitude(Double.parseDouble(pos_long.getText()));
         galam.setYear(Integer.parseInt(year.getText()));
         gd.addGalamsey(galam);
-
-
+        String query = "insert into galamsey(veg_col,col_val,latitude,longitude,year_started) values(?,?,?,?,?)";
+        PreparedStatement pst;
+        try{
+            pst= gd.connect().prepareStatement(query);
+            //  pst.setInt(1,gIDCount); //takes number of question mark and value
+            // System.out.println(gIDCount);
+            pst.setString(1, galam.getVegetation_color()); //takes number of question mark and value
+            System.out.println(galam.getVegetation_color());
+            pst.setInt(2, galam.getColor_value()); //takes number of question mark and value
+            System.out.println(galam.getColor_value());
+            pst.setDouble(3, galam.getLatitude()); //takes number of question mark and value
+            System.out.println(galam.getLatitude());
+            pst.setDouble(4, galam.getLongitude()); //takes number of question mark and value
+            System.out.println(galam.getLongitude());
+            pst.setInt(5, galam.getYear()); //takes number of question mark and value
+            System.out.println(galam.getYear());
+            int count=pst.executeUpdate();// returns a int of the number of rows affected
+            //  gIDCount++;
+            System.out.println(count+ " row/s affected");
+        }catch(Exception e){
+            System.out.println("Error: "+e);
+        }
 
     }
 
