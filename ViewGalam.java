2252ed4feec95a09a64a9e0732b5f2eeb677a02a
melwinkle <50456098+melwinkle@users.noreply.github.com>
2020-02-20 21:47:16 +0000

    Update ViewGalam.java

--- ViewGalam.java
+++ ViewGalam.java
@@ -1,64 +1,76 @@
 package earthquakemonitoring;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import javafx.event.ActionEvent;
+
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-/**
- *
- * @author Richard and Aileen
- */
 public class ViewGalam implements Initializable {
-    private MenuButton back_b;
+
+    @FXML
     private Button stats_id;
     private AnchorPane pane_id;
-    private TableView table_id;
+    @FXML
+    private TableView<TableContent> table_id;
+    @FXML
+    private Button del_id;
+    @FXML
+    private TableColumn avg_id;
+    @FXML
+    private TableColumn n_id;
+    @FXML
     private TableColumn i_id;
+    @FXML
     private TableColumn v_id;
+    @FXML
     private TableColumn c_id;
+    @FXML
     private TableColumn l_id;
     private Button g_id;
-    Galamseydb g_db;
-    TableView[] table;
+    Galamseydb g_db= new Galamseydb();
+    private final ObservableList<TableContent> data = FXCollections.observableArrayList();
 
-    /**
-     *
-     * @param url
-     * @param resourceBundle
-     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       i_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer("id")>);
-       v_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer("Year")>);
-       c_id.setCellValueFactory(new PropertyValueFactory<TableContent,String("obs_name")>);
-       l_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer("area_c")>);
-       
+        i_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer>("id"));
+        n_id.setCellValueFactory(new PropertyValueFactory<TableContent,String>("obs_name"));
+        v_id.setCellValueFactory(new PropertyValueFactory<TableContent,String>("country_name"));
+        c_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer>("years"));
+        l_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer>("area_c"));
+        avg_id.setCellValueFactory(new PropertyValueFactory<TableContent,Double>("avg"));
+        table_id.setItems(getContent());
+
     }
-    
+
     public ObservableList<TableContent> getContent(){
         ArrayList<String[]> list =g_db.getAllObservatory1();
         for(String[] p: list){
-        data.add(new TableContent(Integer.parseInt(p[0]),p[1],p[2],Double.parseDouble(p[3]),Double.parseDouble(p[4]),Integer.parseInt(5)))
+            System.out.println(p[0] +" :"+  p[1] +" :"+ p[2] +" :"+ p[3]+" :"+ p[4]+ ":"+ p[5] );
+            data.add(new TableContent(Integer.parseInt(p[0]),p[1],p[2],
+                    Integer.parseInt(p[3]),Double.parseDouble(p[4]),Double.parseDouble(p[5])));
         }
         return data;
     }
 
-    /**
-     *This method changes the screen to "View_galam.fxml"
-     * @param event
-     * @throws IOException
-     */
+
+
     public void buttonstats (ActionEvent event) throws IOException {
         Parent screen_two= FXMLLoader.load(getClass().getResource("View_galam.fxml"));
         Scene scene = new Scene(screen_two);
@@ -67,31 +79,11 @@ public class ViewGalam implements Initializable {
         s_two.show();
     }
 
-    /**
-     *This method the list of observatory names in list items
-     * @throws IOException
-     * @throws SQLException
-     */
-    public void Dropdown() throws IOException, SQLException {
-        int coun= g_db.getNumberOfObservatory();
-        table = new TableView[coun];
-        for( int i=0; i < coun; i++){
-            table[i] = new TableView();
-            String text= g_db.getObservatory(i++);
-            String[] oWords= text.split(":");
-            i_id.setCellValueFactory(	new	PropertyValueFactory1<Galamseydb,	String>(oWords[0]));
-            v_id.setCellValueFactory(	new	PropertyValueFactory1<Galamseydb,	String>(oWords[1]));
-            c_id.setCellValueFactory(	new	PropertyValueFactory1<Galamseydb, Integer>(Integer.parseInt(oWords[2])));
-            l_id.setCellValueFactory(	new	PropertyValueFactory1<Galamseydb,	Double>(Double.parseDouble(oWords[3])));
 
-        }
-    }
 
-    /**
-     *This method changes the screen to "Observatory_Data.fxml"
-     * @param actionEvent
-     * @throws IOException
-     */
+
+
+
     public void back(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent screen_two= FXMLLoader.load(getClass().getResource("Observatory_Data.fxml"));
         Scene scene = new Scene(screen_two);
@@ -99,12 +91,6 @@ public class ViewGalam implements Initializable {
         s_two.setScene(scene);
         s_two.show();
     }
-
-    /**
-     *This method changes the screen to "List_Event.fxml"
-     * @param actionEvent
-     * @throws IOException
-     */
     public void query(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent screen_two= FXMLLoader.load(getClass().getResource("List_Event.fxml"));
         Scene scene = new Scene(screen_two);
@@ -112,5 +98,31 @@ public class ViewGalam implements Initializable {
         s_two.setScene(scene);
         s_two.show();
     }
-}
+    /**
+     * This deletes a record from the table
+     * @param actionEvent
+     * @throws SQLException
+     */
+    public void delete(javafx.event.ActionEvent actionEvent) throws SQLException, IOException {
+        ObservableList<TableContent> dat = table_id.getSelectionModel().getSelectedItems();
+        int di= dat.get(0).getID();
+        g_db.deleteGalam_Observ(di);
+        g_db.deleteGalamsey(di);
+        Parent screen_two= FXMLLoader.load(getClass().getResource("View_Galam.fxml"));
+        Scene scene = new Scene(screen_two);
+        Stage s_two=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        s_two.setScene(scene);
+        s_two.show();
+    }
 
+    /**
+     * This method gets the average of an observatory selected
+     * @throws SQLException
+     */
+    public void Avg() throws SQLException {
+        ObservableList<TableContent> dat = table_id.getSelectionModel().getSelectedItems();
+        int di= dat.get(0).getID();
+        double avrg = g_db.getAvgObserv(di);
+        avg_id.setText(String.valueOf(avrg));
+    }
+}
