3d7cf2dc0ade762b985de02d5ce1fb365fc483dc
melwinkle <50456098+melwinkle@users.noreply.github.com>
2020-02-20 21:46:47 +0000

    Update GalOb.java

--- GalOb.java
+++ GalOb.java
@@ -1,45 +1,74 @@
 package earthquakemonitoring;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
- *
- * @author Richard and Aileen
+ * @Author Aileen Akpalu
+ * @version 1.0.3
  */
 public class GalOb implements Initializable {
-    MenuButton menu_id;
+
+    @FXML
+    private TableView<TableContent> menu_id;
+    @FXML
+    private TableColumn o_id;
+    @FXML
+    private TableColumn nam_id;
+    @FXML
     Label l;
-    Galamsey galam= new Galamsey();
+
     Galamseydb gd= new Galamseydb();
-    AddNew g= new AddNew();
+
+    private final ObservableList<TableContent> data = FXCollections.observableArrayList();
+
 
     /**
-     *
+     * This method puts the data in the table when the screen loads
      * @param url
      * @param resourceBundle
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        o_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer>("ID"));
+        nam_id.setCellValueFactory(new PropertyValueFactory<TableContent,Integer>("name_o"));
+
+        menu_id.setItems(getContent());
 
     }
 
     /**
-     * This method changes the screen to "GalamObser.fxml"
+     * This method gets the lsit of observatories
+     * @return
+     */
+    public ObservableList<TableContent> getContent(){
+        ArrayList<String[]> list =gd.getAllObservatory1();
+        for(String[] p: list){
+            System.out.println(p[0] +" :"+  p[1] );
+            data.add(new TableContent(Integer.parseInt(p[0]),p[1]));
+        }
+        return data;
+    }
+
+    /**
+     * This method changes the screen to GalamObser
      * @param actionEvent
      * @throws IOException
      */
@@ -51,55 +80,9 @@ public class GalOb implements Initializable {
         s_two.show();
     }
 
-    /**
-     *This method changes the screen to "Add_New.fxml"
-     * @param actionEvent
-     * @throws IOException
-     */
-    public void buttonpush(javafx.event.ActionEvent actionEvent) throws IOException {
-
-        Parent screen_two= FXMLLoader.load(getClass().getResource("Add_New.fxml"));
-        Scene scene = new Scene(screen_two);
-        Stage s_two=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        s_two.setScene(scene);
-        s_two.show();
-    }
 
     /**
-     *This method the list of observatory names in list items
-     * @throws SQLException
-     */
-    public void dropdown() throws SQLException {
-        MenuItem[] menu=new MenuItem[gd.getNumberOfObservatory()];
-        for (int i = 0; i <gd.getNumberOfObservatory() ; i++) {
-            menu_id.getItems().add(i,menu[i]);
-            menu[i]=new MenuItem();
-            String text= gd.getObservatory(i++);
-            String[] oWords= text.split(":");
-            String g_name=oWords[1];
-            menu[i].setText(g_name);
-            menu[i].setStyle("bold");
-            System.out.println(menu[i].getText());
-            menu_id.getItems().add(menu[i]);
-            EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent e)
-                {
-                    l.setText(((MenuItem)e.getSource()).getText() + " selected");
-
-                }
-            };
-            menu[i].setOnAction(event1);
-            int y=Integer.parseInt(oWords[3]);
-            int di=gd.getGalamseyyId();
-            int ig= gd.getObservatoryId(g_name,y);
-            gd.addGalam_Observ(ig,di);
-
-
-        }
-    }
-
-    /**
-     *This method changes the screen to "Add_New.fxml"
+     * This method changes the screen to Add_New
      * @param actionEvent
      * @throws IOException
      */
@@ -110,9 +93,28 @@ public class GalOb implements Initializable {
         s_two.setScene(scene);
         s_two.show();
 
+
+    }
+
+    /**
+     * This method allows a user to select a record in a table
+     * @param actionEvent
+     * @throws SQLException
+     * @throws IOException
+     */
+    public void Selection(ActionEvent actionEvent) throws SQLException, IOException {
+          ObservableList<TableContent> dat = menu_id.getSelectionModel().getSelectedItems();
+          int di= dat.get(0).getID();
+        System.out.println(di);
+          int ig=gd.getGalamseyyId();
+        System.out.println(ig);
+          gd.addGalam_Observ(di,ig);
+        Parent screen_two= FXMLLoader.load(getClass().getResource("Add_New.fxml"));
+        Scene scene = new Scene(screen_two);
+        Stage s_two=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        s_two.setScene(scene);
+        s_two.show();
     }
 
 
-
 }
-
